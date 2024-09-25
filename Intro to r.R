d5916ba8e8af374c366a50b09d4f8c61e4e8ef71
itsalicewainaina <itsalicewainaina@gmail.com>
2024-09-25 19:20:56 +0300

    Intro to R

---
+++ Intro to r.R
@@ -0,0 +1,20 @@
+#USing variables
+first_variable <- "This is my variable"
+second_variable <- 12.5
+#vectors c(x,y,z,....)
+Vec_1 <- c(13, 48.5,71,101.5,2)
+#pipes a tool in R for expressing a sequence of multiple operations, represented with "%>%"
+vec_2 <- c(11,2,y,15,21)
+Vec_3 <- Vec_1[3] + vec_2[3]
+typeof(vec_2)
+length(vec_2)
+is.double(vec_2)
+#lets create a list 
+list("a", 1L, 1.5,TRUE)
+str(list("a", 1L, 1.5, TRUE))
+z <- list(list(list(1 , 3, 5)))
+str(z)
+#Naming lists
+list(list(list('Chicago' = 1, 'New York' = 2, 'Los Angeles' = 3)))
+
+str(list(list(list('Chicago' = 1, 'New York' = 2, 'Los Angeles' = 3))))
